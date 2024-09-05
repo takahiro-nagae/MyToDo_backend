a81e60a07f41c3c2d91cc25d8463f772971f0d0e
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-09-05 21:21:24 +0900

    update user

--- models/users.go
+++ models/users.go
@@ -45,3 +45,14 @@ func GetUser(id int) (user User, err error) {
 
 	return user, err
 }
+
+func (u *User) UpdateUser() (err error) {
+	cmdU := `UPDATE users SET name = ?, email = ? WHERE id = ?`
+	_, err = Db.Exec(cmdU, u.Name, u.Email, u.ID)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return err
+}
