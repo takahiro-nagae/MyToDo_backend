81e915c77550940b8d7662e5b6a371d115a3bb17
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-09-07 14:51:20 +0900

    todo モデル作成

--- main.go
+++ main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"MyToDo_backend/models"
-	"fmt"
 )
 
 func main() {
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
+	// u, _ := models.GetUser(3)
 
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-
-	u.DeleteUser()
-
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	todo, _ := models.GetTodo(3)
+	todo.DeleteTodo()
 }
